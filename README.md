5390adbe25cf622eeecfd7c243e646c08cbc5428
dengcl <dengcl@253.com>
2022-02-17 10:49:58 +0800

    feat: 添加和使用 weui

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # mini-program-wechat-zhihu
+
+仿知乎微信小程序。用于学习实践，数据全部为前端 mock 数据。
+
+## 技术
+
+weui、微信小程序
+
+## 功能
+
+1. 首页推荐
+2. 个人中心
+3. 搜索页
+4. 请求方法封装
+
+## 页面预览
+
+![首页](./images/preview01.jpg)
+
+![搜索](./images/preview02.jpg)
+
+![个人中心](./images/preview03.jpg)
--- pages/user/user.js
+++ pages/user/user.js
@@ -13,6 +13,7 @@ Page({
       { text: '收藏', key: 'collectionCount' },
     ],
     navHeight: 0,
+    visible: false,
   },
 
   getUserInfo: function () {
@@ -44,53 +45,14 @@ Page({
       navHeight: wx.getStorageSync('navInnerHeight')
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleOpen: function () {
+    this.setData({
+      visible: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleClose: function () {
+    this.setData({
+      visible: false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
